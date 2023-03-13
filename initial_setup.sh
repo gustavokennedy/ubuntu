c3b0ccc3199c6cd9d51a4788c39b7d1d2070ab3f
Gustavo Kennedy Renkel <98914036+gustavokennedy@users.noreply.github.com>
2023-03-13 20:27:10 -0300

    Update initial_setup.sh

--- initial_setup.sh
+++ initial_setup.sh
@@ -24,67 +24,70 @@ OTHER_PUBLIC_KEYS_TO_ADD=(
 ### VARIAVEIS LÓGICAS ###
 ####################
 
+echo " _____     _______ ____      _    _     _       ____ _     ___  _   _ ____
+ / _ \ \   / / ____|  _ \    / \  | |   | |     / ___| |   / _ \| | | |  _ \
+| | | \ \ / /|  _| | |_) |  / _ \ | |   | |    | |   | |  | | | | | | | | | |
+| |_| |\ V / | |___|  _ <  / ___ \| |___| |___ | |___| |__| |_| | |_| | |_| |
+ \___/  \_/  |_____|_| \_\/_/   \_\_____|_____(_)____|_____\___/ \___/|____/
+"
+
+echo "Iniciando ambiente..."
+echo "Aidiconando usuário sudo e ajustando privilégios..."
 # Adicione o usuário sudo e conceda privilégios
 useradd --create-home --shell "/bin/bash" --groups sudo "${USERNAME}"
 
+echo "Verificando se conta possui senha real..."
 # Verifique se a conta root tem uma senha real definida
 encrypted_root_pw="$(grep root /etc/shadow | cut --delimiter=: --fields=2)"
 
-if [ "${encrypted_root_pw}" != "*" ]; then
-     # Transfira a senha root gerada automaticamente para o usuário, se presente
-     # e bloqueie a conta root para acesso baseado em senha
-    echo "${USERNAME}:${encrypted_root_pw}" | chpasswd --encrypted
-    passwd --lock root
-else
-     # Exclua a senha inválida do usuário se estiver usando chaves para que uma nova senha
-     # pode ser definido sem fornecer um valor anterior
-    passwd --delete "${USERNAME}"
-fi
-
-# Expire a senha do usuário sudo imediatamente para forçar uma mudança
-chage --lastday 0 "${USERNAME}"
-
+echo "Criando diretório SSH para usuário sudo..."
 # Cria o diretório SSH para o usuário sudo
 home_directory="$(eval echo ~${USERNAME})"
 mkdir --parents "${home_directory}/.ssh"
 
+echo "Copiando chaves autorizadas SSH..."
 # Copie o arquivo `authorized_keys` da raiz, se solicitado
 if [ "${COPY_AUTHORIZED_KEYS_FROM_ROOT}" = true ]; then
     cp /root/.ssh/authorized_keys "${home_directory}/.ssh"
 fi
 
+echo "Adicioando chaves SSH fornecidas..."
 # Adicionar chaves públicas adicionais fornecidas
 for pub_key in "${OTHER_PUBLIC_KEYS_TO_ADD[@]}"; do
     echo "${pub_key}" >> "${home_directory}/.ssh/authorized_keys"
 done
 
+echo "Ajustando permissões e propriedades do SSH..."
 # Ajuste a propriedade e as permissões da configuração SSH
 chmod 0700 "${home_directory}/.ssh"
 chmod 0600 "${home_directory}/.ssh/authorized_keys"
 chown --recursive "${USERNAME}":"${USERNAME}" "${home_directory}/.ssh"
 
-# Desabilita o login root SSH com senha
-sed --in-place 's/^PermitRootLogin.*/PermitRootLogin prohibit-password/g' /etc/ssh/sshd_config
-if sshd -t -q; then
-    systemctl restart sshd
-fi
-
+echo "Instalando Ansible..."
 # Instala Ansible
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt update
 sudo apt install ansible
+echo "---- Ansible instalado com sucesso!"
 
+echo "Instalando Nginx..."
 # Instala Nginx
 sudo apt update
 sudo apt install nginx
+echo "---- Nginx instalado com sucesso!"
 
+echo "Instalando MySQL..."
 # Instala MySQL
 sudo apt install mysql-server
+echo "---- MySQL instalado com sucesso!"
 
+echo "Instalando PHP..."
 # Instala PHP
 sudo apt install php-fpm php-mysql
 sudo systemctl reload nginx
+echo "---- PHP instalado com sucesso!"
 
+echo "Instalando NodeJS com NVM..."
 # Instala NodeJS
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
@@ -92,3 +95,4 @@ source ~/.bashrc
 nvm install lts/*
 nvm use lts/*
 sudo apt install npm
+echo "---- NodeJS & NPM instalados com sucesso!"
