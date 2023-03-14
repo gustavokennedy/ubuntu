8f607e45d7885de9d31352ea37eb1c8388b92354
Gustavo Kennedy Renkel <98914036+gustavokennedy@users.noreply.github.com>
2023-03-13 21:43:21 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -86,7 +86,6 @@ echo "---- PHP instalado com sucesso!"
 echo "Instalando NodeJS com NVM..."
 # Instala NodeJS
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 source ~/.bashrc
 nvm install lts/*
 nvm use lts/*
